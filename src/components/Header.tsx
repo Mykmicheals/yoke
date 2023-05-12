2936bfc4bdf5f97ac166958aa9b4175baa30e833
Andrew Micheal <mykmicheals@gmail.com>
2023-05-12 20:02:51 +0100

    removed some unecessary header code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,55 +1,37 @@
-import React,{useState,} from 'react'
+import React, { useState } from "react";
 
 function Header() {
-
   const [isOpen, setIsOpen] = useState(false);
 
-
-
   return (
-      <div>
-          
-             <div className="relative fixed">
-      {isOpen && (
-        <div
-          onClick={() => setIsOpen(false)}
-          className="w-1/2 backdrop-brightness-50 h-screen fixed top-0 right-0 z-20"
-        ></div>
-      )}
+    <div>
+      <div className="relative fixed">
+        {isOpen && (
+          <div
+            onClick={() => setIsOpen(false)}
+            className="w-1/2 backdrop-brightness-50 h-screen fixed top-0 right-0 z-20"
+          ></div>
+        )}
 
-      <div className=" z-50 bg-teal-500 w-full h-12 px-8 md:px-20 pt-3">
-        <div className="flex gap-4 md:gap-24">
-          {/* <span className="flex gap-2 md:gap-4">
-            <FaPhoneAlt className="text-white mt-1" />
-            <p className="text-white">081-3515-9897</p>
-          </span> */}
-          {/* <span className="flex gap-2 md:gap-4">
-            <AiOutlineMail className="text-white mt-1" />
-            <p className="text-white">myk@gmail.com</p>
-          </span> */}
-        </div>
-      </div>
-      <header className=" w-full bg-white shadow-lg z-10">
-        <div className="container mx-auto px-4">
-          <div className="flex justify-between items-center py-4">
-            {/* <div className="items-center">
-              <img className="w-32 h-12 mr-2" src={logos} alt="School logo" />
-            </div> */}
+        <header className=" w-full bg-white shadow-lg z-10">
+          <div className="container mx-auto px-4">
+            <div className="flex justify-between items-center py-4">
+              <h1>YOKE</h1>
 
-            <div className="md:hidden">
-              <button
-                type="button"
-                className="block text-gray-500 hover:text-gray-900 focus:text-gray-900 focus:outline-none"
-                onClick={() => setIsOpen(!isOpen)}
-              >
-                {/* {!isOpen ? (
+              <div className="md:hidden">
+                <button
+                  type="button"
+                  className="block text-gray-500 hover:text-gray-900 focus:text-gray-900 focus:outline-none"
+                  onClick={() => setIsOpen(!isOpen)}
+                >
+                  {/* {!isOpen ? (
                   <BiMenuAltRight className="w-8 h-8" />
                 ) : (
                   <h2 className=" text-2xl">X</h2>
                 )} */}
-              </button>
+                </button>
 
-              {/* <Transition
+                {/* <Transition
                 show={isOpen}
                 enter="transition ease-out duration-200 transform"
                 enterFrom="-translate-x-full"
@@ -63,13 +45,13 @@ function Header() {
                   <SideBar />
                 </div>
               </Transition> */}
+              </div>
             </div>
           </div>
-        </div>
-      </header>
+        </header>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
