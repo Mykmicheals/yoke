d4f78ab269c50fdb9f64af4ef96f51efb740ee74
Andrew Micheal <mykmicheals@gmail.com>
2023-05-14 10:58:02 +0100

    added fetch weather

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,26 +1,45 @@
-import Map from 'react-map-gl';
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
+import Map, { Popup, Marker } from "react-map-gl";
+import { MdCancel } from "react-icons/md";
 
-
+import { HiLocationMarker } from "react-icons/hi";
+import { useState } from "react";
 
 function MapComponent() {
+  const value = useSelector((state: any) => state.map);
+  const [showPopup, setShowPopup] = useState(false);
 
-  const value = useSelector((state: any) => state.map)
-  
+
 
   return (
-     <Map
-      initialViewState={{
-        longitude: value.lat,
-        latitude: value.lng,
-        zoom: 14
-      }}
-      style={{width: 600, height: 400}}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-      mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+    <div className="mt-24">
+      <Map
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+        style={{ height: "54vh", width: "90%" }}
+        longitude={value.lng}
+        latitude={value.lat}
+        zoom={10}
+      >
+        <Marker onClick={()=>setShowPopup(true)} longitude={value.lng} latitude={value.lat} anchor="bottom">
+          <HiLocationMarker size={32} color="red" />
+        </Marker>
 
-    />
-  )
+           {showPopup ? (
+                  <Popup
+                    className="z-1000 bg-red"
+                    longitude={value.lng}
+                    latitude={value.lat}
+                    anchor="bottom"
+              
+                  >
+               
+                  </Popup>
+                ) : null}
+
+      </Map>
+    </div>
+  );
 }
 
-export default MapComponent
+export default MapComponent;
