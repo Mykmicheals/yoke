eec9b70d762812c682ada6b839feb755ac70c23a
Andrew Micheal <mykmicheals@gmail.com>
2023-05-14 16:07:31 +0100

    added mapbox styles and marker

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -4,12 +4,15 @@ import { MdCancel } from "react-icons/md";
 
 import { HiLocationMarker } from "react-icons/hi";
 import { useState } from "react";
+import WeatherComponent from "./WeatherComponent";
 
 function MapComponent() {
   const value = useSelector((state: any) => state.map);
   const [showPopup, setShowPopup] = useState(false);
 
+  const [zoom, setZoom] = useState(10);
 
+  console.log(value);
 
   return (
     <div className="mt-24">
@@ -19,24 +22,28 @@ function MapComponent() {
         style={{ height: "54vh", width: "90%" }}
         longitude={value.lng}
         latitude={value.lat}
-        zoom={10}
+        zoom={zoom}
       >
-        <Marker onClick={()=>setShowPopup(true)} longitude={value.lng} latitude={value.lat} anchor="bottom">
-          <HiLocationMarker size={32} color="red" />
+        <Marker
+          style={{ zIndex: 10 }}
+          onClick={() => setShowPopup(true)}
+          longitude={value.lng}
+          latitude={value.lat}
+          anchor="bottom"
+        >
+          <HiLocationMarker className="z-10" size={32} color="red" />
         </Marker>
 
-           {showPopup ? (
-                  <Popup
-                    className="z-1000 bg-red"
-                    longitude={value.lng}
-                    latitude={value.lat}
-                    anchor="bottom"
-              
-                  >
-               
-                  </Popup>
-                ) : null}
-
+        {showPopup ? (
+          <Popup
+            className="z-1000 bg-red"
+            longitude={value.lng}
+            latitude={value.lat}
+            anchor="bottom"
+          >
+            <WeatherComponent />
+          </Popup>
+        ) : null}
       </Map>
     </div>
   );
